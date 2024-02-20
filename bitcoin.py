747d9ad2874932fa914c0d03bb73bcb5ba27f821
Thuan Tran <thuantran2704@gmail.com>
2024-02-20 08:20:54 -0500

    see current price

---
+++ bitcoin.py
@@ -0,0 +1,23 @@
+import apikey
+import requests
+
+headers = {
+    'X-CMC_PRO_API_KEY' : apikey.key,
+    'Accepts' : 'application/json',
+    'Accept-Encoding': 'deflate, gzip'
+}
+
+params = {
+    'start' : '1',
+    'limit' : '5',
+    'convert': 'USD'
+}
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+
+data = requests.get(url, params = params, headers = headers ).json()['data']
+
+for coins in data:
+    if(coins['symbol'] == 'BTC'):
+        print(coins['symbol'],coins['quote']['USD']['price'])
